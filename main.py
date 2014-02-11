88c8fb9ece746271a912ccdba648405815b72e5c
The Dude <spectet@esiee.fr>
2014-02-11 23:39:43 +0100

    main : Test Kernel

--- main.py
+++ main.py
@@ -9,7 +9,26 @@ import mean_shift.func as ms
 import random as rand
 import matplotlib.pyplot as plt
 
+# TEST KERNEL NORMAL
+im = np.matrix(range(0,10000))
 
-im = np.matrix(range(0,99))
-im_n = ms.kernel(im)
+plt.subplot(221)
+plt.plot(im.A1)
+im_n = ms.kernel( np.multiply(im-5000,im-5000)/1000000.)
+plt.subplot(222)
+plt.plot(im_n.A1)
+
+# TEST KERNEL EPANECHNIKOV
+im_bis = np.matrix(range(0,1000)).astype(float)
+im_bis -= 500
+im_bis /= 250.
+
+plt.subplot(223)
+plt.plot(im_bis.A1)
+im_n = ms.kernel( im_bis, normal=0)
+plt.subplot(224)
+plt.plot(im_n.A1)
+
+
+plt.show()
 
