a5ad2e1bb9ee7fda7f082bf33306c45c1c21d88e
Geozz <giry.geoffrey@gmail.com>
2014-02-20 15:16:55 +0100

    Fixed video, add video sample

--- video/test.py
+++ video/test.py
@@ -2,30 +2,33 @@
 """
 """
 
+import cv2.cv as cv
 import cv2
 import numpy as np
 
 def toto(path):
     # setup video capture
     cap = cv2.VideoCapture(path)
+    
+    print cap
 
     # get frame, store in array
-    while True:
+    while(cap.isOpened()):
         ret, im = cap.read()
         # The frame is in im
-        
+
         #make process here
-        
+        im = cv2.GaussianBlur(im, (0, 0), 5)
+
         # Display
         cv2.imshow('video test', im)
         
-        #todo fix quit key 
-        key = cv2.waitKey(10)
-        print key       
-        print ord('q')
-        
-        if key == ord('q'):
+        # Use Q to quit
+        if cv2.waitKey(25) & 0xFF == ord('q'):
             break
+    
+    cap.release()
+    cv2.destroyAllWindows()
 
 
 ##############################################################################
@@ -33,7 +36,8 @@ def toto(path):
 ##############################################################################    
     
 def test_video():
-    toto(0)
+    toto('./car.mp4')
+    #toto(0)
     
 if __name__ == "__main__":
     test_video()
